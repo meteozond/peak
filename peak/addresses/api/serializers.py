20282ffbf277666edbf25f1264ab290e77a47c37
Andrey Sukhov <sukhov.sav@gmail.com>
2021-07-20 08:58:21 +0300

    Изменил AddressSerializer: атрибуты поля city

--- peak/addresses/api/serializers.py
+++ peak/addresses/api/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 
+from django.utils.translation import gettext_lazy as _
+
 from ..models import Address
 from ..validators import validate_city
 
 
 class AddressSerializer(serializers.ModelSerializer):
-    city = serializers.CharField(max_length=50, validators=[validate_city, ])
+    city = serializers.CharField(
+        max_length=50,
+        validators=[validate_city, ],
+        label=_('Город'),
+        help_text=_('Город (например, Bratsk. Лучше на английском, не было времени '
+                    'разбираться с cities-light'),
+    )
 
     class Meta:
         model = Address
